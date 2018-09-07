1519d1f2a2f006fd727bc86fcea27b4de35d8aad
da-lucky <da.ru.stp@gmail.com>
2018-09-07 19:13:57 +0300

    reworked input handling

--- commonDefs.hpp
+++ commonDefs.hpp
@@ -13,9 +13,11 @@ constexpr int LOCAL_PORT_TO_LISTEN = 30000;
 
 constexpr std::size_t MAX_NUM_SESSIONS_ALLOWED = 3;
     
-constexpr std::size_t MAX_BUF_SIZE = 1;
+constexpr std::size_t MAX_BUF_SIZE = 10;
 
 constexpr std::size_t CTRL_CMD_SIZE = 3;
+
+constexpr std::size_t MAX_USER_CMD_SIZE = MAX_BUF_SIZE;
 
 inline std::string formErrnoString(const char* s) {
     std::stringstream ss{s};
@@ -24,10 +26,15 @@ inline std::string formErrnoString(const char* s) {
 }
 
 constexpr char IAC = '\xff';
+constexpr char CR {'\x0d'};
+constexpr char LF {'\x0a'};
 
-const std::string SPACE_TAB{'\x20','\x09'};
+constexpr char SPACE {'\x20'};
+constexpr char TAB {'\x09'};
 
-const std::string CR_LF{'\xd','\xa'};
+const std::string CR_LF{CR, LF};
+
+const std::string SPACE_TAB{SPACE, TAB};
 
 
 }
--- session.cpp
+++ session.cpp
@@ -24,81 +24,72 @@ const std::string defaultPrompt(std::string("\n") + std::string(APP_NAME) + std:
 
 thread_local std::queue<std::string> cmdQueue {};
 thread_local InputBuffer_T inputBuffer {};
-thread_local std::string cmdPart {};
 thread_local int session_socket {};
 
+thread_local std::string USERcmdPart {};
 thread_local std::string CTRLcmdPart {};
 
 /*---------------------------- functions definitions -----------------------------------------------------------------*/
-void receiveCmd_streambuf() {
+void handleSymbol(char symbol) {
+    switch (symbol) {
+        case '\x7f' : {
+            std::cout << "BS\n";
+            if(! USERcmdPart.empty()) {
+                USERcmdPart.pop_back();
+            }
+            break;
+        }
+        default: {
+            USERcmdPart.push_back(symbol);
+            break;
+        }
+    }
+}
 
-    std::array<char, CTRL_CMD_SIZE> tmp_ctrl;
-    std::string tmp_inp;
-    tmp_inp.reserve(MAX_BUF_SIZE);
-    
+/* read input in telnet char mode */
+void readInput() {
+
+    std::stringstream ss;
+
     while (true) {
-
         const int recvBytes = recv(session_socket, inputBuffer.data(), inputBuffer.size(), 0);        
 
         if(-1 != recvBytes) {
-
-            tmp_inp.clear();
-
-            std::stringstream ss;
+            
             ss.rdbuf()->pubsetbuf(inputBuffer.data(), recvBytes);
 
             while(ss.rdbuf()->in_avail()) {
-
+              
                 if(! CTRLcmdPart.empty()) {
-                    
-                    auto b = ss.readsome(tmp_ctrl.begin(), CTRL_CMD_SIZE - CTRLcmdPart.size());
+                    CTRLcmdPart.push_back(ss.get());
 
-                    CTRLcmdPart.append(tmp_ctrl.begin(), b);
-
-                    if(CTRLcmdPart.size() == CTRL_CMD_SIZE) {
-                        cmdQueue.push(CTRLcmdPart);
-                        CTRLcmdPart.clear();                        
+                    if(CTRL_CMD_SIZE == CTRLcmdPart.size()) {
+                        cmdQueue.emplace(CTRLcmdPart);
+                        CTRLcmdPart.clear();
                     }
                     continue;
-                }   
+                }
 
-                char symbol = ss.get();
+                char symbol = ss.get();                
                 if(IAC == symbol) {
-
-                    tmp_ctrl[0] = symbol;
-
-                    auto b = ss.readsome(&tmp_ctrl[1], CTRL_CMD_SIZE - 1) + 1;
-
-                    if(b < CTRL_CMD_SIZE) {
-                        CTRLcmdPart.append(tmp_ctrl.begin(), b);
-                    } else {
-                        cmdQueue.emplace(tmp_ctrl.begin(), b);
-                    }                    
-                } else {
-                    if('\x0a' == symbol || '\x00' == symbol) {
-                        break;
-                    } else if('\x0d' == symbol) {
-                        cmdQueue.push(cmdPart); 
-                        cmdPart.clear(); 
-                    } else {
-                        cmdPart.append(1, symbol);  
+                    CTRLcmdPart.push_back(symbol);
+                } else {                                        
+                    if(CR == symbol) {
+                        cmdQueue.emplace(USERcmdPart);
+                        USERcmdPart.clear();
+                        continue;
                     }
+                    if(LF == symbol || '\00' == symbol) { continue; }
 
-                    while(! ss.eof()) {
-    
-                        std::getline(ss, tmp_inp);
-                        if(ss.eof()) {
-                            cmdPart.append(tmp_inp);
-                            tmp_inp.clear();
-                        }
-                        if(! tmp_inp.empty()) {
-                            tmp_inp.pop_back();
-                            cmdQueue.push(tmp_inp); 
-                            cmdPart.clear();
-                        }
+                    handleSymbol(symbol);
+
+                    if(MAX_USER_CMD_SIZE == USERcmdPart.size()) {
+                        USERcmdPart.clear();
+                        std::cerr << "not allowed command length... skip it... MAX allowed " << MAX_USER_CMD_SIZE << "\n";
+                        continue;
                     }
                 }
-            } 
+            }
 
             if(! cmdQueue.empty()) {
                 break;
@@ -109,92 +100,6 @@ void receiveCmd_streambuf() {
         }
     }
 }
-#if 0
-std::string copyToStringAndAdvance(const char* &it, std::size_t dist) {
-    std::string s{it, dist};
-    std::advance(it, dist);
-    return s;
-}
-
-std::string assembleCmd(const std::string& s) {
-
-    std::string c{};
-
-    if(! cmdPart.empty()) { 
-        c.swap(cmdPart);
-    }
-    return c.append(s);
-}
-
-void receiveCmd_CTRL() {
-    while (true) {
-
-        const int recvBytes = recv(session_socket, inputBuffer.data(), inputBuffer.size(), 0);
-
-        if(-1 != recvBytes) {            
-            
-            auto beginIt = inputBuffer.cbegin();
-            auto endIt = beginIt + recvBytes;
-
-            while(beginIt != endIt) {
-                // telnet control commands obtaining
-                if( ! CTRLcmdPart.empty() ) {
-
-                    auto partLen = CTRLcmdPart.size();
-                    std::size_t dist = std::distance(beginIt, endIt);
-
-                    CTRLcmdPart.append(copyToStringAndAdvance(beginIt, std::min((CTRL_CMD_SIZE - partLen), dist)));                    
-
-                    if(CTRLcmdPart.size() == CTRL_CMD_SIZE) {
-                        cmdQueue.push(CTRLcmdPart);
-                        CTRLcmdPart.clear();
-                    }
-                    break;
-                }
-                if( IAC == *beginIt ) {
-                    if (std::distance(beginIt, endIt) < CTRL_CMD_SIZE) {
-
-                        CTRLcmdPart.append(copyToStringAndAdvance(beginIt, std::distance(beginIt, endIt)));
-                        
-                    } else {
-                        cmdQueue.push(copyToStringAndAdvance(beginIt, CTRL_CMD_SIZE));                                              
-                    }
-                } else {
-                    // user input obtaining
-                    // find end of the input command marked with CR LF
-                    auto crlfPos = std::search(beginIt, endIt, CR_LF.cbegin(), CR_LF.cend());
-
-                    if (crlfPos != endIt) {
-                    
-                        cmdQueue.push(assembleCmd(copyToStringAndAdvance(beginIt, std::distance(beginIt, crlfPos))));
-                    
-                        std::advance(beginIt, CR_LF.size());
-
-                        continue;
-                    } else {
-                        cmdPart.append(copyToStringAndAdvance(beginIt, std::distance(beginIt, endIt)));
-                        
-                        auto crlfPos = cmdPart.find(CR_LF);
-                        
-                        if(std::string::npos != crlfPos) {
-                            cmdQueue.push(cmdPart.substr(0, crlfPos));
-                            cmdPart.clear();
-                        }
-                    }
-                }
-           }
-
-            if(! cmdQueue.empty()) {
-                break;
-            }
-
-        } else {
-            std::cerr << formErrnoString("recv returned -1:");
-            return; // TODO: check this!!!            
-        }
-    }
-}
-#endif
 
 void sendResponse(const std::string& response) {
     if(-1 == send(session_socket, response.c_str(), response.size(), 0)) {
@@ -204,6 +109,18 @@ void sendResponse(const std::string& response) {
 
 void prompt() {    
     send(session_socket, defaultPrompt.c_str(), defaultPrompt.size(),0);
+}
+
+void setenv() {
+    constexpr char e1[] = {'\xff','\xfb','\x03'}; // set char mode
+    send(session_socket, e1, sizeof(e1), 0);
+
+    constexpr char e2[] = {'\xff','\xfb','\x01'}; // set char mode
+    send(session_socket, e2, sizeof(e2), 0);
+
+//    constexpr char e3[] = {'\xff','\xfd','\x1f'}; // set char mode
+//    send(session_socket, e3, sizeof(e3), 0);
+
 }
 
    
@@ -254,12 +171,14 @@ void handler(int sock, bool& isActiveFlag) {
     
     session_socket = sock; // init thread local variable for socket
     CTRLcmdPart.reserve(CTRL_CMD_SIZE);
+    USERcmdPart.reserve(MAX_USER_CMD_SIZE);
 
+    setenv();
     prompt();
 
     while(isActiveFlag) {
         
-        receiveCmd_streambuf();
+        readInput();
         
         while (! cmdQueue.empty()) {
 
